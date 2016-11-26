950ba6eabad4e93d6664abba887d36a4bf6dfef0
Lokesh M <lokeshm27@gmail.com>
2016-11-26 16:58:12 +0530

    Added Operations.exit method

--- src/Operations.java
+++ src/Operations.java
@@ -12,17 +12,18 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
+import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.io.PrintWriter;
 import java.net.InetAddress;
 import java.net.ServerSocket;
 import java.net.Socket;
-
 import javax.swing.JButton;
 import javax.swing.JCheckBox;
 import javax.swing.JComboBox;
@@ -41,36 +42,34 @@ public class Operations {
 	private ServerSocket server;
 	private Socket socket;
 	private final File dat = new File("C:\\ProgramData\\SpeakTime\\Data.dat");
-	private boolean openFrame = false;
-	private boolean optionFrame = false;
-	private boolean aboutFrame = false;
-	
+	private boolean isOpen = false, isOption = false, isAbout = false, isDisable = false, isExit = false;
+	private JFrame optionFrame, aboutFrame, openFrame, exitFrame, disableFrame;
+	private BufferedReader br;
+	private PrintWriter pw;
+	private PopupMenu popup;
+	private MenuItem disable;
+	private MenuItem enable;
 	Data data = new Data();
-	
+
 	public void initTray() {
 		try {
 			if (!SystemTray.isSupported()) {
 				return;
 			}
-			final PopupMenu popup = new PopupMenu();
+			popup = new PopupMenu();
 			SystemTray tray = SystemTray.getSystemTray();
 			Image bi = Toolkit.getDefaultToolkit().getImage("res/sm.png");
 
 			ActionListener exitListener = new ActionListener() {
 				public void actionPerformed(ActionEvent e) {
-					try {
-						server.close();
-					} catch (IOException e1) {
-						e1.printStackTrace();
-					}
-					flag.delete();
-					System.exit(0);
+					exit();
 				}
 			};
-			
+
 			ActionListener defaultListener = new ActionListener() {
 				public void actionPerformed(ActionEvent e) {
 					System.out.println("Action performed : default");
+					open();
 				}
 			};
 
@@ -84,24 +83,42 @@ public class Operations {
 			ActionListener syncListener = new ActionListener() {
 				public void actionPerformed(ActionEvent e) {
 					System.out.println("Action performed : sync");
+					sync();
 				}
 			};
 
 			ActionListener aboutListener = new ActionListener() {
 				public void actionPerformed(ActionEvent e) {
 					System.out.println("Action performed : About");
+					about();
 				}
 			};
 
 			ActionListener openListener = new ActionListener() {
 				public void actionPerformed(ActionEvent e) {
-					System.out.println("Action performed : About");
+					System.out.println("Action performed : open");
+					open();
 				}
 			};
 
 			ActionListener testListener = new ActionListener() {
 				public void actionPerformed(ActionEvent e) {
-					System.out.println("Action performed : About");
+					System.out.println("Action performed : Test");
+					test();
+				}
+			};
+
+			ActionListener disableListener = new ActionListener() {
+				public void actionPerformed(ActionEvent e) {
+					System.out.println("Action performed : Disable");
+					disable();
+				}
+			};
+
+			ActionListener enableListener = new ActionListener() {
+				public void actionPerformed(ActionEvent e) {
+					System.out.println("Action performed : enable");
+					enable();
 				}
 			};
 
@@ -117,11 +134,17 @@ public class Operations {
 			MenuItem about = new MenuItem("About Speak-Time");
 			about.addActionListener(aboutListener);
 
-			MenuItem open = new MenuItem("Open Speak-time.");
+			MenuItem open = new MenuItem("Open Speak-time");
 			open.addActionListener(openListener);
 
-			MenuItem test = new MenuItem("Test Audio now.");
+			MenuItem test = new MenuItem("Test Audio now");
 			test.addActionListener(testListener);
+
+			disable = new MenuItem("Disable Speak-Time temporarily");
+			disable.addActionListener(disableListener);
+
+			enable = new MenuItem("Enable now");
+			enable.addActionListener(enableListener);
 
 			popup.add(open);
 			popup.add(options);
@@ -129,6 +152,7 @@ public class Operations {
 
 			popup.add(test);
 			popup.add(sync);
+			popup.add(disable);
 			popup.addSeparator();
 
 			popup.add(about);
@@ -145,78 +169,91 @@ public class Operations {
 		}
 	}
 
-	@SuppressWarnings("resource")
-	private void serial(Data ip){
-		try{
-			ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream(dat));
-			oos.writeObject(ip);
-		} catch (Exception e){
-			
+	public void showTrayMsg(String msg, String title, TrayIcon.MessageType type) {
+		if ((msg != null) && (title != null)) {
+			trayIcon.displayMessage(msg, title, type);
 		}
 	}
-	
+
 	@SuppressWarnings("resource")
-	private Data deserial(){
+	private void serial(Data ip) {
+		try {
+			ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream(dat));
+			oos.writeObject(ip);
+		} catch (Exception e) {
+
+		}
+	}
+
+	@SuppressWarnings("resource")
+	private Data deserial() {
 		Data temp = new Data();
-		try{
+		try {
 			ObjectInputStream ois = new ObjectInputStream(new FileInputStream(dat));
 			temp = (Data) ois.readObject();
-		} catch (Exception e){
-			
+		} catch (Exception e) {
+
 		}
 		return temp;
 	}
-	
+
 	public void init() {
-		if(!dat.exists()){
+		if (!dat.exists()) {
 			serial(new Data());
 		}
 		data = deserial();
 	}
-	
-	public void open(){
-		
+
+	public void disable() {
+		//// TODO
 	}
-	
-	public void test(){
-		
+
+	public void enable() {
+		//// TODO
 	}
-	
-	public void sync(){
-		
+
+	public void open() {
+		//// TODO
 	}
-	
-	public void options(){
-		if(optionFrame){
+
+	public void test() {
+		//// TODO
+	}
+
+	public void sync() {
+		//// TODO
+	}
+
+	public void options() {
+		if (isOption) {
+			optionFrame.toFront();
 			return;
 		}
-		optionFrame = true;
-		JFrame frame = new JFrame("Speak-Time | Options");
-		frame.setDefaultCloseOperation(JFrame.DO_NOTHING_ON_CLOSE);
-		frame.addWindowListener(new WindowAdapter() {
+		optionFrame = new JFrame("Speak-Time | Options");
+		optionFrame.setDefaultCloseOperation(JFrame.DO_NOTHING_ON_CLOSE);
+		optionFrame.addWindowListener(new WindowAdapter() {
 			@Override
-			public void windowClosing(WindowEvent we){
-				if(data.confirm){
-				int res = JOptionPane.showConfirmDialog(null, "Are you sure to exit?\nChanges may not be saved.!", "Confirm Exit.", JOptionPane.YES_NO_OPTION, JOptionPane.WARNING_MESSAGE);
-					if(res == JOptionPane.YES_OPTION){
-						frame.dispose();
-						optionFrame = false;
-					}
+			public void windowClosing(WindowEvent we) {
+				int res = JOptionPane.showConfirmDialog(null, "Are you sure to exit?\nChanges may not be saved.!",
+						"Confirm Exit.", JOptionPane.YES_NO_OPTION, JOptionPane.WARNING_MESSAGE);
+				if (res == JOptionPane.YES_OPTION) {
+					optionFrame.dispose();
+					isOption = false;
 				}
 			}
 		});
-		
+
 		JLabel label1 = new JLabel("Speak time every");
 		SpinnerNumberModel spinner = new SpinnerNumberModel(10, 5, 55, 5);
-		String options[] = {"Minutes", "Hours"};
+		String options[] = { "Minutes", "Hours" };
 		JComboBox<Object> combo = new JComboBox<Object>(options);
-		if(data.minutes>=60){
-			spinner.setValue(data.minutes/60);
+		if (data.minutes >= 60) {
+			spinner.setValue(data.minutes / 60);
 			spinner.setMaximum(5);
 			spinner.setStepSize(1);
 			spinner.setMinimum(1);
 			combo.setSelectedIndex(1);
-		}else{
+		} else {
 			spinner.setMinimum(5);
 			spinner.setStepSize(5);
 			spinner.setValue(data.minutes);
@@ -224,24 +261,24 @@ public class Operations {
 			combo.setSelectedIndex(0);
 		}
 		combo.addActionListener(new ActionListener() {
-			
+
 			@Override
 			public void actionPerformed(ActionEvent arg0) {
 				int sel = combo.getSelectedIndex();
-				if(sel == 0){
+				if (sel == 0) {
 					spinner.setMinimum(5);
 					spinner.setStepSize(5);
-					if(data.minutes<60){
+					if (data.minutes < 60) {
 						spinner.setValue(data.minutes);
-					}else{
+					} else {
 						spinner.setValue(10);
 					}
 					spinner.setMaximum(59);
-				}else{
+				} else {
 					spinner.setStepSize(1);
-					if(data.minutes>=60){
-						spinner.setValue(data.minutes/60);
-					}else{
+					if (data.minutes >= 60) {
+						spinner.setValue(data.minutes / 60);
+					} else {
 						spinner.setValue(1);
 					}
 					spinner.setMaximum(5);
@@ -249,60 +286,70 @@ public class Operations {
 				}
 			}
 		});
-		
+
 		JCheckBox cb1 = new JCheckBox("Start Speak-Time on start-up.");
 		JCheckBox cb2 = new JCheckBox("Don't speak time if audio is playing.");
 		JCheckBox cb3 = new JCheckBox("ALways confirm before exiting.");
 		cb1.setSelected(data.autoStart);
 		cb2.setSelected(data.ignore);
-		
+		cb3.setSelected(data.confirm);
+
 		JButton save = new JButton("Save");
 		save.addActionListener(new ActionListener() {
-			
+
 			@Override
 			public void actionPerformed(ActionEvent arg0) {
-				int mins;
-				Data temp;
-				
-				if(combo.getSelectedIndex() == 0)
-				{
-					mins = (Integer) spinner.getValue();
-					if((mins%5 == 0) && (mins >= 1) && (mins <= 55)){
-						temp = new Data(mins, cb1.isSelected(), cb3.isSelected(), cb2.isSelected());
-						data = temp;
-						sync();
-						serial(temp);
-					}else{
-						JOptionPane.showMessageDialog(null,"Invlaid number for minutes.\nPlease try again.", "Error - Speak-Time", JOptionPane.ERROR_MESSAGE);
-					}
-				}else{
-					mins = (Integer) spinner.getValue();
-					if((mins<=5) && (mins>=1)){
-						temp = new Data((mins*60), cb1.isSelected(), cb3.isSelected(), cb2.isSelected());
-						data = temp;
-						sync();
-						serial(temp);
-					}else{
-						JOptionPane.showMessageDialog(null,"Invlaid number for hours.\nPlease try again.", "Error - Speak-Time", JOptionPane.ERROR_MESSAGE);
+				int res = JOptionPane.showConfirmDialog(null, "Are you sure to save changes.?", "Confirm Save",
+						JOptionPane.YES_NO_OPTION, JOptionPane.WARNING_MESSAGE);
+				if (res == JOptionPane.YES_OPTION) {
+					int mins;
+					Data temp;
+
+					if (combo.getSelectedIndex() == 0) {
+						mins = (Integer) spinner.getValue();
+						if ((mins % 5 == 0) && (mins >= 1) && (mins <= 55)) {
+							temp = new Data(mins, cb1.isSelected(), cb2.isSelected(), cb3.isSelected());
+							data = temp;
+							sync();
+							serial(temp);
+							isOption = false;
+							optionFrame.dispose();
+						} else {
+							JOptionPane.showMessageDialog(null, "Invlaid number for minutes.\nPlease try again.",
+									"Error - Speak-Time", JOptionPane.ERROR_MESSAGE);
+						}
+					} else {
+						mins = (Integer) spinner.getValue();
+						if ((mins <= 5) && (mins >= 1)) {
+							temp = new Data((mins * 60), cb1.isSelected(), cb2.isSelected(), cb3.isSelected());
+							data = temp;
+							sync();
+							serial(temp);
+							isOption = false;
+							optionFrame.dispose();
+						} else {
+							JOptionPane.showMessageDialog(null, "Invlaid number for hours.\nPlease try again.",
+									"Error - Speak-Time", JOptionPane.ERROR_MESSAGE);
+						}
 					}
 				}
 			}
 		});
-		
-		
+
 		JButton cancel = new JButton("Cancel");
 		cancel.addActionListener(new ActionListener() {
-			
+
 			@Override
 			public void actionPerformed(ActionEvent arg0) {
-				int res = JOptionPane.showConfirmDialog(null, "Are you sure to cancel?\nChanges may not be saved.!", "Confirm Exit", JOptionPane.YES_NO_OPTION, JOptionPane.WARNING_MESSAGE);
-				if(res == JOptionPane.YES_OPTION){
-					frame.dispose();
-					optionFrame = false;
+				int res = JOptionPane.showConfirmDialog(null, "Are you sure to cancel?\nChanges may not be saved.!",
+						"Confirm Exit", JOptionPane.YES_NO_OPTION, JOptionPane.WARNING_MESSAGE);
+				if (res == JOptionPane.YES_OPTION) {
+					isOption = false;
+					optionFrame.dispose();
 				}
 			}
 		});
-		
+
 		JPanel p1 = new JPanel(new GridBagLayout());
 		GridBagConstraints gbc = new GridBagConstraints();
 		gbc.insets = new Insets(10, 10, 10, 10);
@@ -310,65 +357,148 @@ public class Operations {
 		gbc.gridx = GridBagConstraints.RELATIVE;
 		gbc.gridy = 0;
 		gbc.anchor = GridBagConstraints.NORTH;
-		
+
 		p1.add(label1, gbc);
 		p1.add(new JSpinner(spinner), gbc);
 		p1.add(combo, gbc);
-		
+
 		JPanel p2 = new JPanel(new GridBagLayout());
 		GridBagConstraints g2 = new GridBagConstraints();
 		g2.insets = new Insets(5, 5, 5, 5);
 		g2.gridx = 0;
 		g2.gridy = GridBagConstraints.RELATIVE;
 		g2.anchor = GridBagConstraints.WEST;
-		
+
 		p2.add(cb1, g2);
 		p2.add(cb2, g2);
 		p2.add(cb3, g2);
-		
+
 		JPanel p3 = new JPanel(new GridBagLayout());
 		GridBagConstraints g3 = new GridBagConstraints();
 		g3.insets = new Insets(5, 10, 5, 10);
-		
+
 		g3.gridx = 0;
 		g3.gridy = 0;
 		p3.add(save, g3);
-		
+
 		g3.gridx = 1;
 		g3.gridy = 0;
 		p3.add(cancel, g3);
-		
+
 		JPanel p4 = new JPanel(new GridBagLayout());
 		GridBagConstraints g4 = new GridBagConstraints();
 		g4.insets = new Insets(5, 5, 5, 5);
-		
+
 		g4.gridx = 0;
 		g4.gridy = 0;
 		p4.add(p1, g4);
-		
+
 		g4.gridx = 0;
 		g4.gridy = 1;
 		p4.add(p2, g4);
-		
+
 		g4.gridx = 0;
 		g4.gridy = 2;
 		p4.add(p3, g4);
-		
-		frame.add(p4, BorderLayout.CENTER);
-		frame.setSize(300, 400);
-		frame.setResizable(true);
-		frame.setLocationRelativeTo(null);
-		frame.setVisible(true);
+
+		optionFrame.add(p4, BorderLayout.CENTER);
+		optionFrame.setSize(300, 400);
+		optionFrame.setResizable(true);
+		optionFrame.setLocationRelativeTo(null);
+		isOption = true;
+		optionFrame.setVisible(true);
 	}
-	
-	private int showConfirmDialog() {
-		return 0;
+
+	public void exit() {
+		if (!data.confirm) {
+			try {
+				server.close();
+				flag.delete();
+				System.exit(0);
+			} catch (Exception e) {
+
+			}
+		} else {
+			if (isExit) {
+				exitFrame.toFront();
+				return;
+			}
+			exitFrame = new JFrame("Confirm exit.!");
+			exitFrame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+
+			JLabel label1 = new JLabel("Are you sure to exit Speak-Time.?");
+
+			JCheckBox cb1 = new JCheckBox("Always confirm before exiting");
+			cb1.setSelected(data.confirm);
+			cb1.addActionListener(new ActionListener() {
+				@Override
+				public void actionPerformed(ActionEvent arg0) {
+					data = new Data(data.minutes, data.autoStart, data.ignore, cb1.isSelected());
+					serial(data);
+				}
+			});
+
+			JPanel p1 = new JPanel(new GridBagLayout());
+			GridBagConstraints g1 = new GridBagConstraints();
+			g1.insets = new Insets(5, 5, 5, 5);
+			g1.gridx = 0;
+			g1.gridy = GridBagConstraints.RELATIVE;
+			g1.anchor = GridBagConstraints.WEST;
+
+			p1.add(label1, g1);
+			p1.add(cb1, g1);
+
+			JButton yes = new JButton("Yes");
+			yes.addActionListener(new ActionListener() {
+
+				@Override
+				public void actionPerformed(ActionEvent arg0) {
+					try {
+						exitFrame.dispose();
+						isExit = false;
+						server.close();
+						flag.delete();
+						System.exit(0);
+					} catch (Exception e) {
+
+					}
+				}
+			});
+
+			JButton no = new JButton("No");
+			no.addActionListener(new ActionListener() {
+				@Override
+				public void actionPerformed(ActionEvent arg0) {
+					exitFrame.dispose();
+					isExit = false;
+				}
+			});
+
+			JPanel p2 = new JPanel(new GridBagLayout());
+			GridBagConstraints g2 = new GridBagConstraints();
+			g2.insets = new Insets(5, 15, 25, 15);
+
+			g2.gridx = 0;
+			g2.gridy = 0;
+			p2.add(yes, g2);
+
+			g2.gridx = 1;
+			g2.gridy = 0;
+			p2.add(no, g2);
+
+			exitFrame.add(p1, BorderLayout.NORTH);
+			exitFrame.add(p2, BorderLayout.SOUTH);
+			exitFrame.setSize(300, 175);
+			exitFrame.setLocationRelativeTo(null);
+			exitFrame.setVisible(true);
+		}
 	}
-	
-	public void about(){
-		
+
+	public void about() {
+		//// TODO
 	}
-	
+
+	@SuppressWarnings("resource")
 	public boolean isWorking() {
 		try {
 			File Dir = new File("C:\\ProgramData\\SpeakTime");
@@ -382,6 +512,24 @@ public class Operations {
 			}
 
 			if (flag.exists()) {
+				try {
+					InetAddress localhost = InetAddress.getLocalHost();
+					Socket socket = new Socket(localhost, PORT);
+					br = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+					pw = new PrintWriter(socket.getOutputStream(), true);
+					pw.println("Confirm");
+					pw.flush();
+					String inputLine;
+					if ((inputLine = br.readLine()) != null) {
+						if (inputLine.equals("Affirmative")) {
+							System.out.println("Server running confirmed.");
+							socket.close();
+							return true;
+						}
+					}
+				} catch (Exception e) {
+					return false;
+				}
 				return true;
 			} else {
 				flag.createNewFile();
@@ -401,7 +549,7 @@ public class Operations {
 				@Override
 				public void run() {
 					try {
-						while(true){
+						while (true) {
 							new IPCThread(server.accept()).start();
 						}
 					} catch (Exception e) {
@@ -430,16 +578,14 @@ public class Operations {
 		}
 	}
 
-	
 	public void onClose() {
-		try{
+		try {
 			flag.delete();
 			server.close();
 			System.exit(0);
-		} catch (Exception e){
-			
+		} catch (Exception e) {
+
 		}
 	}
 
-	
 }
